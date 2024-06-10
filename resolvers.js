58cffbaf473c5577ce985eb9c5b655af09f1d77e
Julien Millaud <millaud.julien@gmail.com>
2024-06-10 11:28:54 +0200

    [FEAT] connection appollo

--- resolvers.js
+++ resolvers.js
@@ -1,23 +1,25 @@
 const users = [
-    { id: '1', name: 'Alice', email: 'alice@example.com', age: 25, city: 'Paris' },
-    { id: '2', name: 'Bob', email: 'bob@example.com', age: 30, city: 'Lyon' },
-    { id: '3', name: 'Charlie', email: 'charlie@example.com', age: 35, city: 'Marseille' }
-  ];
-  
-  const addUser = (_, { name, email, age, city }) => {
-    const newUser = { id: generateUserId(), name, email, age, city };
-    users.push(newUser);
-    return newUser;
-  };
-  
-  const resolvers = {
-    Query: {
-      // Résolveurs pour les requêtes existantes
-    },
-    Mutation: {
-      addUser,
-    },
-  };
-  
-  
-  
+  { id: '1', name: 'Alice', email: 'alice@example.com', age: 25, city: 'Paris' },
+  { id: '2', name: 'Bob', email: 'bob@example.com', age: 30, city: 'Lyon' },
+  { id: '3', name: 'Charlie', email: 'charlie@example.com', age: 35, city: 'Marseille' }
+];
+
+const generateUserId = () => (users.length + 1).toString();
+
+const addUser = (_, { name, email, age, city }) => {
+  const newUser = { id: generateUserId(), name, email, age, city };
+  users.push(newUser);
+  return newUser;
+};
+
+const resolvers = {
+  Query: {
+    user: (_, { id }) => users.find(user => user.id === id),
+    users: () => users,
+  },
+  Mutation: {
+    addUser,
+  },
+};
+
+module.exports = resolvers;
